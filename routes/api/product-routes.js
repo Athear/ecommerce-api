c36cf16d49f544e3e8094ebcb474e29eba12620f
Todd Malone <toddomalone@gmail.com>
2021-03-18 16:43:05 -0500

    removed unneeded comments

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,8 +7,6 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
   try{
     const categories = await Product.findAll({
       include:[Category,Tag]
@@ -22,8 +20,6 @@ router.get('/', async (req, res) => {
 
 // get one product
 router.get('/:id', async (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
   try{
     const product = await Product.findByPk(req.params.id,{
       include:[Category,Tag]
@@ -123,7 +119,6 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete one product by its `id` value
   try{
     const product = await Product.findByPk(req.params.id,{
       include:[Tag]
