58aceb69dfa1b4fe5cbfdecf794822b0b2408a6a
Todd Malone <toddomalone@gmail.com>
2021-03-18 22:23:31 -0500

    corrected foreign keys for product-category relationship

--- models/index.js
+++ models/index.js
@@ -4,8 +4,8 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-Category.hasMany(Product)
-Product.belongsTo(Category)
+Category.hasMany(Product,{foreignKey:'category_id'})
+Product.belongsTo(Category,{foreignKey:'category_id'})
 
 Product.belongsToMany(Tag, {through:ProductTag,foreignKey:'tag_id'})
 Tag.belongsToMany(Product, {through:ProductTag,foreignKey:'product_id'})
