0386e9ffbfbc218fd309f91cf76dfb8044e7d0db
Todd Malone <toddomalone@gmail.com>
2021-03-18 21:15:47 -0500

    modified tag-routes GETs to exclude data from ProductTag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,22 +2,36 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
+//TODO: ProductTag is duplicating fields
 
 router.get('/', async (req, res) => {
   try{
     const categories = await Tag.findAll({
-      include:[Product]
+      include:[{model:Product, through:{attributes:{exclude:['product_id','tag_id']}}},]
     });
     res.status(200).json(categories);
     }
     catch(err){
+      console.log(err) //DEBUG
       res.status(500).json(err);
     }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try{
+    const product = await Tag.findByPk(req.params.id,{
+      include:[{model:Product, through:{attributes:{exclude:['product_id','tag_id']}}},]
+    });
+    if(product){
+      res.status(200).json(product);  
+    }else{
+      res.status(404).json(`No tag with ID ${req.params.id} found`);  
+    }    
+  }
+  catch(err){
+    //console.log(err) //DEBUG
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
