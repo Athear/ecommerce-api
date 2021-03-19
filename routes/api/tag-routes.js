f4a7d464b88f840e5350f0ff6ce59e1f6a063449
Todd Malone <toddomalone@gmail.com>
2021-03-18 21:43:26 -0500

    DELETE route set up in tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -80,8 +80,23 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try{
+    const removedStatus = await Tag.destroy({
+      where:{
+        id:req.params.id 
+      }
+    });
+      if(removedStatus>0){
+        res.status(200).json(`Tag ${req.params.id} removed`);  
+      }else{
+        res.status(404).json(`Tag ${req.params.id} does not exist`);  
+      }    
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
